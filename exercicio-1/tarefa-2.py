797adfaae8fc37a882208ec600ba4982dc2652bd
Fabricio Anibal <fabricio_anibal@hotmail.com>
2019-02-20 21:51:08 -0300

    Executei a tarefa 2

---
+++ exercicio-1/tarefa-2.py
@@ -0,0 +1,6 @@
+num = int(input("Insira um numero inteiro "))
+contador = 0
+while contador <=10:
+    result = num * contador
+    print(num,"X", contador, "=", result, )
+    contador = contador + 1
