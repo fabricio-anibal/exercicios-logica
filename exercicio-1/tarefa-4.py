5a8b955a52d230f209e895b5703fb34ee32e655a
Fabricio Anibal <fabricio_anibal@hotmail.com>
2019-02-28 23:33:57 -0300

    Consertei a tarefa 4

--- exercicio-1/tarefa-4.py
+++ exercicio-1/tarefa-4.py
@@ -1,18 +1,7 @@
-################################################################################
-
-# author: Fabricio Anibal Pinheiro Alves
-# GitHub: https://github.com/fabricio-anibal
-
-################################################################################
-
-# Desenvolva um algoritmo que solicite um número inteiro positivo N ao usuário e imprima a soma dos N primeiros números pares maiores do que zero.
-
-################################################################################
-
 n = 0
 soma = 0
 while n <= 0:
     n = int(input("Entre com o valor inteiro positivo"))
-for i in range (2, (n*2)+1 , 2):
+for i in range (2, n+1 , 2):
     soma = i + soma
 print(soma)
